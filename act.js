b2fda237fcf3b9a846c6bb5c511da45ef3f36551
Liliana D´Aquino <126512554+44Daq@users.noreply.github.com>
2023-11-13 20:37:18 -0300

    pass repeat

--- act.js
+++ act.js
@@ -1,8 +1,20 @@
-const BOTON_REGISTRO=document.getElementById("botonRegistro");
+function comprobarClave() {
+    clave1= document.getElementById('clave1');
+    clave2= document.getElementById('clave');
 
-BOTON_REGISTRO.addEventListener("click",() => {
+}
+ 
+    const BOTON_REGISTRO=document.getElementById("botonRegistro");
+
+    BOTON_REGISTRO.addEventListener("click",() => {
+    
+          if (clave1.value == clave2.value) {
+            alert("Se ha registrado correctamente");
+          } else  {
+           alert("La contraseña no coincide");
+          }
+          }
+        
+    )   
+    
    
-        alert("Se ha registrado correctamente");
-        
-        
-    })
